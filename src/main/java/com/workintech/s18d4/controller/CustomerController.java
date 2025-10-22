1f863d2ed1e62eae5e406fba7431cfc072c779ff
Erdinç ÖZGÜL <erdincozgul.tr@gmail.com>
2025-10-22 18:42:39 +0300

    Final

---
+++ src/main/java/com/workintech/s18d4/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.workintech.s18d4.controller;
+
+import com.workintech.s18d4.dto.CustomerResponse;
+import com.workintech.s18d4.entity.Customer;
+import com.workintech.s18d4.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAll() {
+        return customerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer get(@PathVariable Long id) {
+        return customerService.find(id);
+    }
+
+    @PostMapping
+    public CustomerResponse save(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.save(customer);
+        return new CustomerResponse(savedCustomer.getId(), savedCustomer.getEmail(), savedCustomer.getSalary());
+    }
+
+    @DeleteMapping
+    public CustomerResponse delete(@RequestBody Customer customer) {
+        Customer deletedCustomer = customerService.save(customer);
+        return new CustomerResponse(deletedCustomer.getId(), deletedCustomer.getEmail(), deletedCustomer.getSalary());
+    }
+}
